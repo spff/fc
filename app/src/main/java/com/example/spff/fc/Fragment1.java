b212f2981bf5ca6f86c6742eda2099f87aa93c20
spff <spff@bitmessage.ch>
2017-01-27 12:34:23 +0800

    add swipe delete to listview

--- app/src/main/java/com/example/spff/fc/Fragment1.java
+++ app/src/main/java/com/example/spff/fc/Fragment1.java
@@ -1,39 +1,37 @@
 package com.example.spff.fc;
 
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.BaseAdapter;
 import android.widget.Button;
-import android.view.View.OnClickListener;
+import android.widget.Checkable;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
-import android.widget.ListView;
-import android.widget.ImageView;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.widget.Filterable;
-import android.widget.Filter;
-import android.widget.Checkable;
-import android.net.Uri;
 import android.widget.Toast;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.AdapterView.OnItemLongClickListener;
 
-import android.widget.AdapterView;
-import android.support.v7.app.AlertDialog;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.GregorianCalendar;
-import java.util.Map;
-import java.util.HashMap;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -99,6 +97,28 @@ public class Fragment1 extends Fragment {
                 return true;
             }
         });
+
+        SwipeDismissListViewTouchListener touchListener =
+                new SwipeDismissListViewTouchListener(
+                        listView,
+                        new SwipeDismissListViewTouchListener.DismissCallbacks() {
+                            @Override
+                            public boolean canDismiss(int position) {
+                                return true;
+                            }
+
+                            @Override
+                            public void onDismiss(ListView listView, int[] reverseSortedPositions) {
+                                for (int position : reverseSortedPositions) {
+
+                                    items.remove(position);
+                                    adapter.notifyDataSetChanged();
+
+                                }
+
+                            }
+                        });
+        listView.setOnTouchListener(touchListener);
 
 
         Button addButton = (Button) view.findViewById(R.id.add_button);
